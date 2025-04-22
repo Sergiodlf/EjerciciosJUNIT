de1b6282df42074f88d7813d0186642e44d23df3
Sergio <sergio4vientos@gmail.com>
2025-04-22 13:34:48 +0200

    Creación Proyecto

---
+++ src/main/java/org/cuatrovientos/dam/ed/EjerciciosJUNIT/App.java
@@ -0,0 +1,13 @@
+package org.cuatrovientos.dam.ed.EjerciciosJUNIT;
+
+/**
+ * Hello world!
+ *
+ */
+public class App
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
